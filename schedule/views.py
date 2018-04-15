1eabda31720f5847405b3adb64fdce2104261dab
Mike Rosengrant <mike@rosengrant.me>
2018-04-15 16:52:35 -0400

    Created board partial template

--- schedule/views.py
+++ schedule/views.py
@@ -14,15 +14,14 @@ class DepartureView(TemplateView):
     """Displays the Departure Boards for North and South Station.
     """
     template_name = "schedule/departures.html"
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(DepartureView, self).get_context_data(*args, **kwargs)
-        api_wrapper = DepartureAPIWrapper()
-        station_departures = api_wrapper.get_station_departures()
-        context['north_station_departures'] = station_departures['North Station']
-        context['south_station_departures'] = station_departures['South Station']
-        context['timestamp'] = station_departures["North Station"][1]["TimeStamp"]
-        return context
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(DepartureView, self).get_context_data(*args, **kwargs)
+    #     api_wrapper = DepartureAPIWrapper()
+    #     station_departures = api_wrapper.get_station_departures()
+    #     context['north_station_departures'] = station_departures['North Station']
+    #     context['south_station_departures'] = station_departures['South Station']
+    #     context['timestamp'] = station_departures["North Station"][1]["TimeStamp"]
+    #     return context
 
 def departure_api_view(request):
     """Get data from MBTA Website."""
