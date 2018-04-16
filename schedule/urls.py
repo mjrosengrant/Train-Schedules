42e1c6919cd49c8b2f812805ca26e1ae8bb490d2
Mike Rosengrant <mike@rosengrant.me>
2018-04-16 11:54:35 -0400

    Simplified url patterns

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 
 from schedule import views
 
 
 urlpatterns = [
-    url(r'^departures/', include([
-        url(r'^$', views.DepartureView.as_view(), name="departures"),
-        url(r'^api/$', views.station_departure_api_view, name="api"),
-    ]))
+    url(r'^$', views.DepartureView.as_view(), name="departures"),
+    url(r'^api/$', views.station_departure_api_view, name="api"),
 ]
